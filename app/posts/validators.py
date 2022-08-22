49510a9054cff04c09bb5b4f70ab78d07649e930
LTaeng <ksx0330@naver.com>
2022-08-22 23:20:28 +0900

    Delete app directory

--- app/posts/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError 
-
-def validateContentType(value):
-    types = ["text", "image", "code", "error"]
-    if value not in types:
-        raise ValidationError("Type Error")
